a2b3154cbfa000e6699084e7823399e062d265b5
Sylvain Thery <sylvain.thery@icube.unistra.fr>
2019-03-13 08:26:42 +0400

    failue count

--- src/test.java
+++ src/test.java
@@ -3,12 +3,13 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class test {
-	public static int main(String[] args) {
+	public static void main(String[] args) {
 		Result result = JUnitCore.runClasses(TestSuite1.class);
 		for (Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
 		}
-		return result.getFailureCount();
+		if (result.getFailureCount()>0)
+			throw new EmptyStackException();
 	}
 }
 
